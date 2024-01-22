ff2764615cc30952e1de984a13275d57d9c4e4ab
Oliver Chen <oliver_chen@berkeley.edu>
2024-01-22 11:08:18 -0800

    Update train.py

--- train.py
+++ train.py
@@ -1,26 +1,24 @@
-# Import necessary libraries
-import numpy as np  # Add this line to import numpy
+
+import numpy as np  
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 import joblib
 
-# Import the data extraction function from getdata.py
 from getdata import extract_features_labels
 
-# Load the data
+# Load  data
 features, labels = extract_features_labels('annotations.json')
 
-# Choose a machine learning model
 model = RandomForestClassifier()
 
 print("model defined")
 
-# Train the model
+# Train
 model.fit(features, labels)
 
-# Evaluate the model
+# Evaluate 
 scores = cross_val_score(model, features, labels, cv=5)
 print("Cross-validated Accuracy:", np.mean(scores))
 
-# Save the trained model
+# Save 
 joblib.dump(model, 'jiu_jitsu_pose_model.pkl')
